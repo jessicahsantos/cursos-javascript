940f944ba1fb9662acf94b42ae9cde0fafa7f85a
Jessica Santos <jessica16hs@gmail.com>
2023-01-26 13:16:51 -0300

    subtrair e somar, braco

--- robotron-2000/main.js
+++ robotron-2000/main.js
@@ -1,8 +1,12 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", dizOi)
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1
+})
 
-function dizOi() {
-    console.log("0i")
-}
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1
+})
 
