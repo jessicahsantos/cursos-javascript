ae582be281bf287ac1360b9286facde69af5e76a
Jessica Santos <jessica16hs@gmail.com>
2023-03-08 08:05:15 -0300

    loops

--- explorando-linguagem-javascript/loops.js
+++ explorando-linguagem-javascript/loops.js
@@ -7,29 +7,10 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
-const
-
+const temPassagemComprada = true;
 
 console.log("Destinos possiveis:");
 console.log(listaDeDestinos);
-
-//if(idadeComprador >= 18){
-    //console.log("Comprador maior de idade")
-   // listaDeDestinos.splice(1,1); //removendo item
-//} else if(estaAcompanhada){
-    //console.log("Comprador está acompanhado");
-    //listaDeDestinos.splice(1,1); //removendo item
-//}else{
-    //console.log("Não é maior de Idade e não posso vender.")
-//}
-//console.log(listaDeDestinos)
-
-
-//console.log(idadeComprador > 18);
-//console.log(idadeComprador < 18);
-//console.log(idadeComprador <= 18);
-//console.log(idadeComprador >= 18);
-//console.log(idadeComprador == 18);
 
 if(idadeComprador >= 18){
     console.log("Comprador maior de idade")
@@ -40,5 +21,4 @@ if(idadeComprador >= 18){
 }else{
     console.log("Não é maior de Idade e não posso vender.")
 }
-
 
