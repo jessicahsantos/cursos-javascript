6e5d88a4562014532324da75fef0ab8f6c907db3
Jessica Santos <jessica16hs@gmail.com>
2023-03-08 15:26:19 -0300

    true ou false

--- explorando-linguagem-javascript/loops.js
+++ explorando-linguagem-javascript/loops.js
@@ -2,12 +2,13 @@ console.log(`\n Trabalhando com  condicionais`);
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
-    `Rio de Janeiro`,
+    `Rio de Janeiro`
 );
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
+const destino = "São Paulo";
 
 console.log("\n Destinos possiveis:");
 console.log(listaDeDestinos);
@@ -15,3 +16,14 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada
 == true;
 
+let contador = 0;
+let destinoExiste = false;
+while(contador<3){
+
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+console.log("Destino existe:", destinoExiste);
