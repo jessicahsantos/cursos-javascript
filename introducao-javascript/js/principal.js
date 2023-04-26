7cb8133a4954a237ed08abbec90efd17a7c49ceb
Jessica Santos <jessica16hs@gmail.com>
2023-04-26 06:35:45 -0300

    adicionando nova tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -37,4 +37,44 @@ for(var i = 0; i < pacientes.length ; i++) {
       var imc = peso / (altura * altura);
       tdImc.textContent = imc.toFixed(2);
     }
-  }
+}
+
+var botaoadicionar = document.querySelector("#adicionar-paciente");
+botaoadicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    console.log(pacienteTr);
+
+});
+
+titulo.addEventListener("click",function (){
+  console.log("Eu fui clicado");
+})
+
