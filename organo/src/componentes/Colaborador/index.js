eee901423c16a07329e42aa1006de32357c36945
Jessica Santos <jessica16hs@gmail.com>
2023-08-30 09:47:15 -0300

    Adicionando Colaborador

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (<div>
+    <div>
+        <img src='https://github.com/jessicahsantos.png' alt='Jessica Santos' />
+        <img src='https://github.com/igortrinidad.png' alt='Igor Trindade' />
+      </div>
+      <div>
+        <h4>Jéssica Santos</h4>
+        <h5>Instrutora</h5>
+        <h4>Igor Trindade</h4>
+        <h5>Instrutor</h5>
+    </div>
+  </div>)
+}
+
+export default Colaborador
